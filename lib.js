c82dab32e519f565ebf328b152ffaccebd492970
zira <ziraproton@proton.me>
2026-02-22 17:13:25 -0500

    Fix lib.js

--- lib.js
+++ lib.js
@@ -1,4 +1,4 @@
-if(localStorage.getItem('browser') === null ) {
+if(localStorage.getItem('searchengine') === null ) {
     localStorage.setItem('searchengine','duckduckgo')
 }
 
@@ -15,23 +15,28 @@ function search() {
     else if(localStorage.getItem('searchengine') === 'google') {
         window.location.href = `https://google.com/search?q=${url}&safe=active&ssui=on`;
     }
-
+        
     else if(localStorage.getItem('searchengine') === 'duckduckgo') {
         window.location.href = `https://duckduckgo.com/?q=${url}&ia=web`;
-
+    }
+        
     else if(localStorage.getItem('searchengine') === 'startpage') {
         window.location.href = `https://www.startpage.com/sp/search?query=${url}`
-
+    }
+        
     else if(localStorage.getItem('searchengine') === 'vyntr') {
         window.location.href = `https://vyntr.com/search?q=${url}`
-
+    }
+        
     else if(localStorage.getItem('searchengine') === 'ecosia') {
         window.location.href = `https://www.ecosia.org/search?method=index&q=${url}`
-
+    }
+        
     else if(localStorage.getItem('searchengine') === 'brave') {
         window.location.href = `https://search.brave.com/search?q=${url}`
-
-    else if(localStorage.getItem('searchengine') === 'yandex' {
+    }
+        
+    else if(localStorage.getItem('searchengine') === 'yandex') {
         window.location.href = `https://yandex.com/search/?text=${url}`
     }
 }
